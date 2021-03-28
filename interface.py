7eb20092029d32d15696348e0537de00242268f1
Tony <antonmakeev18@gmail.com>
2021-03-28 22:04:36 +0300

    добавлен загрузочный экран, обновлён интерфейс

--- interface.py
+++ interface.py
@@ -1,12 +1,36 @@
 import sys
-
-from PyQt5 import QtWidgets
-
+from PyQt5 import QtWidgets, QtGui, QtCore
+from PyQt5.QtCore import QTimer
 from logic import Calculator
 from main_ui import Ui_MainWindow
+from progressbar_ui import Ui_LoardingScreen
+import time
 
 
-# pyuic5 main_ui.ui -o main_ui.py
+class LoardingScreen(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(LoardingScreen, self).__init__()
+
+        self.ui = Ui_LoardingScreen()
+        self.ui.setupUi(self)
+
+        # автоматический запуск программы
+        QtCore.QTimer.singleShot(1500, lambda: self.doAction())
+
+        # УДАЛЯЕМ РАМКИ
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_AttributeCount)
+
+    def doAction(self):
+        for i in range(101):
+            time.sleep(0.05)
+            self.ui.progressBar.setProperty("value", i)
+            if i == 100:
+                time.sleep(2)
+                self.main_window = MyWindow()
+                self.main_window.show()
+                self.close()
+
 
 class MyWindow(QtWidgets.QMainWindow, Calculator):
     def __init__(self):
@@ -30,48 +54,54 @@ class MyWindow(QtWidgets.QMainWindow, Calculator):
         self.ui.btn_converter_2.clicked.connect(self.showConverter)
 
         # счёт единиц длины
-        self.ui.btn_count.clicked.connect(lambda: self.getLength(value_1=self.ui.cb_units_of_length.currentText(), # единица из которой переводят
-                                                                 value_2=self.ui.cb_units_of_length_2.currentText(), # единица в которую переводят
-                                                                 number_1=self.ui.le_units_of_length.text(), # первое число для перевода
-                                                                 number_2=self.ui.le_units_of_length_2.text())) # второе число для перевода
+        self.ui.btn_count.clicked.connect(
+            lambda: self.getLength(value_1=self.ui.cb_units_of_length.currentText(),  # единица из которой переводят
+                                   value_2=self.ui.cb_units_of_length_2.currentText(),  # единица в которую переводят
+                                   number_1=self.ui.le_units_of_length.text(),  # первое число для перевода
+                                   number_2=self.ui.le_units_of_length_2.text()))  # второе число для перевода
 
         # счёт единиц массы
-        self.ui.btn_count_2.clicked.connect(lambda: self.getMass(value_1=self.ui.cb_units_of_mass.currentText(), # единица из которой переводят
-                                                                 value_2=self.ui.cb_units_of_mass_2.currentText(),  # единица в которую переводят
-                                                                 number_1=self.ui.le_units_of_mass.text(), # первое число для перевода
-                                                                 number_2=self.ui.le_units_of_mass_2.text())) # второе число для перевода
+        self.ui.btn_count_2.clicked.connect(
+            lambda: self.getMass(value_1=self.ui.cb_units_of_mass.currentText(),  # единица из которой переводят
+                                 value_2=self.ui.cb_units_of_mass_2.currentText(),  # единица в которую переводят
+                                 number_1=self.ui.le_units_of_mass.text(),  # первое число для перевода
+                                 number_2=self.ui.le_units_of_mass_2.text()))  # второе число для перевода
 
         # счёт единиц площади
-        self.ui.btn_count_3.clicked.connect(lambda: self.getSquare(value_1=self.ui.cb_units_of_square.currentText(), # единица из которой переводят
-                                                                   value_2=self.ui.cb_units_of_square_2.currentText(), # единица в которую переводят
-                                                                   number_1=self.ui.le_units_of_square.text(), # первое число для перевода
-                                                                   number_2=self.ui.le_units_of_square_2.text())) # второе число для перевода
+        self.ui.btn_count_3.clicked.connect(
+            lambda: self.getSquare(value_1=self.ui.cb_units_of_square.currentText(),  # единица из которой переводят
+                                   value_2=self.ui.cb_units_of_square_2.currentText(),  # единица в которую переводят
+                                   number_1=self.ui.le_units_of_square.text(),  # первое число для перевода
+                                   number_2=self.ui.le_units_of_square_2.text()))  # второе число для перевода
 
         # счёт единиц времени
-        self.ui.btn_count_4.clicked.connect(lambda: self.getTime(value_1=self.ui.cb_units_of_time.currentText(), # единица из которой переводят
-                                                                 value_2=self.ui.cb_units_of_time_2.currentText(), # единица в которую переводят
-                                                                 number_1=self.ui.le_units_of_time.text(), # первое число для перевода
-                                                                 number_2=self.ui.le_units_of_time_2.text())) # второе число для перевода
+        self.ui.btn_count_4.clicked.connect(
+            lambda: self.getTime(value_1=self.ui.cb_units_of_time.currentText(),  # единица из которой переводят
+                                 value_2=self.ui.cb_units_of_time_2.currentText(),  # единица в которую переводят
+                                 number_1=self.ui.le_units_of_time.text(),  # первое число для перевода
+                                 number_2=self.ui.le_units_of_time_2.text()))  # второе число для перевода
 
         # счёт единиц давления
         self.ui.btn_count_5.clicked.connect(lambda: self.getPressure(value=self.ui.cb_units_of_pressures.currentText()))
 
         # перевод единиц длины
-        self.ui.btn_conversion_1.clicked.connect(lambda: self.conversionLength(number=self.ui.le_units_of_length_3.text(),
-                                                                               value_1=self.ui.cb_units_of_length_3.currentText(),
-                                                                               value_2=self.ui.cb_units_of_length_4.currentText()))
+        self.ui.btn_conversion_1.clicked.connect(
+            lambda: self.conversionLength(number=self.ui.le_units_of_length_3.text(),
+                                          value_1=self.ui.cb_units_of_length_3.currentText(),
+                                          value_2=self.ui.cb_units_of_length_4.currentText()))
         # перевод единиц площпди
-        self.ui.btn_conversion_2.clicked.connect(lambda: self.conversionSquare(number=self.ui.le_units_of_square_3.text(),
-                                                                              value_1=self.ui.cb_units_of_square_3.currentText(),
-                                                                              value_2=self.ui.cb_units_of_square_4.currentText()))
+        self.ui.btn_conversion_2.clicked.connect(
+            lambda: self.conversionSquare(number=self.ui.le_units_of_square_3.text(),
+                                          value_1=self.ui.cb_units_of_square_3.currentText(),
+                                          value_2=self.ui.cb_units_of_square_4.currentText()))
         # перевод единиц массы
         self.ui.btn_conversion_3.clicked.connect(lambda: self.conversionMass(number=self.ui.le_units_of_mass_3.text(),
-                                                                            value_1=self.ui.cb_units_of_mass_3.currentText(),
-                                                                            value_2=self.ui.cb_units_of_mass_4.currentText()))
+                                                                             value_1=self.ui.cb_units_of_mass_3.currentText(),
+                                                                             value_2=self.ui.cb_units_of_mass_4.currentText()))
         # перевод единиц времени
         self.ui.btn_conversion_4.clicked.connect(lambda: self.conversionTime(number=self.ui.le_units_of_time_3.text(),
-                                                                            value_1=self.ui.cb_units_of_time_3.currentText(),
-                                                                            value_2=self.ui.cb_units_of_time_4.currentText()))
+                                                                             value_1=self.ui.cb_units_of_time_3.currentText(),
+                                                                             value_2=self.ui.cb_units_of_time_4.currentText()))
 
     # настройка показа окон
     # страница конвертера единиц
@@ -93,4 +123,3 @@ class MyWindow(QtWidgets.QMainWindow, Calculator):
     # страница конвертора 2
     def showConverter(self):
         self.ui.stackedWidget.setCurrentWidget(self.ui.unit_converter_page_2)
-
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from interface import MyWindow
+from interface import LoardingScreen
 import sys
 from PyQt5 import QtWidgets
 
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
-    application = MyWindow()
+    application = LoardingScreen()
     application.show()
     sys.exit(app.exec_())
+
+# pyuic5 progressbar_ui.ui -o progressbar_ui.py
+# pyuic5 main_ui.ui -o main_ui.py
+# pyinstaller main.py -w --onefile
+
